e3070226ed02813d318cc63596e67ad102b9f198
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-10-18 21:20:19 +0700

    init

---
+++ go.mod
@@ -0,0 +1,2 @@
+module github.com/Pupan-cpe/Go-learning
+go 1.17
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/Pupan-cpe/Go-learning/variables"
+)
+
+func main() {
+
+	variables.Learn()
+
+
+}
